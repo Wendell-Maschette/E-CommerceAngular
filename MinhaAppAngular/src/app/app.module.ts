ed2697a8c9dde15106f174202b6b349ee3211f9c
Wendell Maschette <wendell.maschette@bitz.com.br>
2021-03-24 15:30:14 -0300

    implementa a navegacao do site

--- MinhaAppAngular/src/app/app.module.ts
+++ MinhaAppAngular/src/app/app.module.ts
@@ -1,24 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
 import { HomeComponent } from './navegacao/home/home.component';
 import { FooterComponent } from './navegacao/footer/footer.component';
+import { rootRouterConfig } from './app.routes';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    SobreComponent,
+    ContatoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    [RouterModule.forRoot(rootRouterConfig, {useHash: false})]
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
